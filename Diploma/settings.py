c51018718bb2e00196e459fe6fff4d0a2894cd5a
YaroslavYaryk <duhanov2003@gmail.com>
2024-05-11 15:47:52 +0300

    docker fix 2

--- Diploma/settings.py
+++ Diploma/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-t1t^ibwwv!bg9vd2w6_+s&07s1u$bj6dw=3&=-671p3+__nq2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
